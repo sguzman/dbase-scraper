f63293c21b92058cc8554406640bb3fc5da123f9
Salvador Guzman <guzmansalv@gmail.com>
2018-09-25 01:37:16 -0700

    multicore

--- src/main.py
+++ src/main.py
@@ -13,10 +13,25 @@ pool = Pool(cores)
 limit = 15865
 
 
+def insert_vids(cursor, data, table_chan):
+    sql_insert_chann = 'INSERT INTO youtube.channels.channels (channel_id) VALUES (%s)'
+
+    for datum in data:
+        if datum not in table_chan:
+            cursor.execute(sql_insert_chann, [datum])
+
+
 def seen_daemon():
+    conn = psycopg2.connect(user='root', password='', host='127.0.0.1', port='5432', database='youtube')
+    table_chans = get_incumbent_chans(conn)
+
     while True:
-        op, msg = seen.get(block=True)
-        print(op, msg)
+        idx, channels = seen.get(block=True)
+        print(channels)
+        cursor = conn.cursor()
+        insert_vids(cursor, channels, table_chans)
+        conn.commit()
+        cursor.close()
 
 
 def page(idx):
@@ -30,10 +45,10 @@ def soup_page(idx):
     json_data = json.loads(text)
     body = json_data['body']['spf_content']
 
-    return bs4.BeautifulSoup(body)
+    return bs4.BeautifulSoup(body, 'html.parser')
 
 
-def send(i):
+def vids(i):
     pg = soup_page(i)
     channels = []
     select = pg.select('a.aj.row')
@@ -44,10 +59,25 @@ def send(i):
     seen.put((i, channels))
 
 
+def get_incumbent_chans(conn):
+    postgresql_select_query = 'SELECT channel_id FROM youtube.channels.channels'
+    cursor = conn.cursor()
+    cursor.execute(postgresql_select_query)
+    records = cursor.fetchall()
+
+    ignore = set()
+    for i in records:
+        ignore.add(i[0])
+
+    print(len(ignore), 'channels from table')
+
+    cursor.close()
+    return ignore
+
+
 def main():
     threading.Thread(target=seen_daemon, daemon=True).start()
-    for i in range(limit):
-        send(i)
+    pool.map(vids, range(limit))
     print('done')
 
 
