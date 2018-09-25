2a1d20d09be00bed67180113fa1fef166beca0ef
Salvador Guzman <guzmansalv@gmail.com>
2018-09-24 23:29:27 -0700

    retrieve channel ids

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import requests
 import queue
 import threading
 import psycopg2
+import json
 from multiprocessing.dummy import Pool
 
 
@@ -11,14 +12,11 @@ cores = 8
 pool = Pool(cores)
 limit = 15865
 
-is_all = []
-
 
 def seen_daemon():
     while True:
         op, msg = seen.get(block=True)
         print(op, msg)
-        is_all.append(msg == 200)
 
 
 def page(idx):
@@ -26,15 +24,31 @@ def page(idx):
     return requests.get(url)
 
 
+def soup_page(idx):
+    req = page(idx)
+    text = req.text
+    json_data = json.loads(text)
+    body = json_data['body']['spf_content']
+
+    return bs4.BeautifulSoup(body)
+
+
 def send(i):
-    seen.put((i, page(i).status_code))
+    pg = soup_page(i)
+    channels = []
+    select = pg.select('a.aj.row')
+    for a_href in select:
+        path = a_href['href'].split('/')
+        channels.append(path[2])
+
+    seen.put((i, channels))
 
 
 def main():
     threading.Thread(target=seen_daemon, daemon=True).start()
-    pool.map(send, range(limit))
+    for i in range(limit):
+        send(i)
     print('done')
-    print(all(is_all))
 
 
 main()
